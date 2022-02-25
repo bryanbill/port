f2a297ef5232c97127722f1c5df73686b79c4dbc
bryanbill <emobrya@gmail.com>
2022-02-25 09:25:13 +0300

    feat:colors - configured tailwind to my brand colors

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+              "dark": "#030304",
+              "grey": "#FCFBFB",
+              "brown": "#482D23",
+              "red": "#EC0D13",
+            }
+        }
+    }
+}
