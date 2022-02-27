91a2e0cb08e0b6b67b1f0a306291d862df279e35
bryanbill <emobrya@gmail.com>
2022-02-27 11:05:16 +0300

    feat:theme-icon - switch icon based on the theme

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 const changeTheme = () => {
     document.getElementById("projects").classList.toggle("projects");
     document.documentElement.classList.toggle('dark');
+
+    // Switch the theme icon
+    const themeIcon = document.getElementById("theme-icon");
+    if (themeIcon.classList.contains("fa-sun")) {
+        themeIcon.classList.remove("fa-sun");
+        themeIcon.classList.add("fa-moon");
+    } else {
+        themeIcon.classList.remove("fa-moon");
+        themeIcon.classList.add("fa-sun");
+    }
 }
 
 
